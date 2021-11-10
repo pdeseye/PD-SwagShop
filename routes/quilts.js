6c728f0828982393c169bd0b54a098613f524187
Payal Desai <payal@idesai.com>
2021-11-10 15:46:03 -0500

    navlinks

--- routes/quilts.js
+++ routes/quilts.js
@@ -4,7 +4,7 @@ const router = Router()
 
 router.get('/new', isLoggedIn, quiltsCtrl.new)
 router.post('/', quiltsCtrl.create)
-router.get('/', quiltsCtrl.index)
+router.get('/', isLoggedIn, quiltsCtrl.index)
 router.get('/:id/edit', isLoggedIn, quiltsCtrl.edit)
 router.get('/:id', quiltsCtrl.show)
 router.put('/:id', isLoggedIn, quiltsCtrl.update)
